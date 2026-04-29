0a4119f9d0c6a35dae3ee73ac6424f27c3c67283
Simon <simsimland2004@gmail.com>
2026-04-29 12:04:48 +0200

    test

--- codeql-custom-queries-java/cleanActual.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-shopt -s globstar
-
-rm **/*.actual
-
-./updateReadme.sh
--- launchTests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-(cd codeql-custom-queries-cpp && ./launchTests.sh)
-(cd codeql-custom-queries-java && ./launchTests.sh)
-(cd codeql-custom-queries-python && ./launchTests.sh)
-(cd codeql-custom-queries-actions && ./launchTests.sh)
-(cd codeql-custom-queries-javascript && ./launchTests.sh)
-
